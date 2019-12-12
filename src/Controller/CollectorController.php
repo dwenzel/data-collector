e5ee8191b1400c3555767a3a7904d571adf2fafc
Dirk Wenzel <wenzel@cps-it.de>
2019-12-12 10:57:38 +0100

    services, dto and controller added

---
+++ src/Controller/CollectorController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace DWenzel\DataCollector\Controller;
+
+use DWenzel\DataCollector\Service\ApiServiceInterface;
+use DWenzel\DataCollector\Service\StorageServiceInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2019 Dirk Wenzel <wenzel@cps-it.de>
+ *  All rights reserved
+ *
+ * The GNU General Public License can be found at
+ * http://www.gnu.org/copyleft/gpl.html.
+ * A copy is found in the text file GPL.txt and important notices to the license
+ * from the author is found in LICENSE.txt distributed with these scripts.
+ * This script is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+/**
+ * Class CollectorController
+ * 
+ * collects data using an ApiService and 
+ * pushes them to a storage
+ */
+class CollectorController extends AbstractController
+{
+    /**
+     * @var ApiServiceInterface 
+     */
+    private $apiService;
+
+    /**
+     * @var StorageServiceInterface
+     */
+    private $storageService;
+
+    public function __construct(ApiServiceInterface $apiService, StorageServiceInterface $storageService)
+    {
+        $this->apiService = $apiService;
+    }
+
+}
---
+++ src/Service/StorageServiceInterface.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace DWenzel\DataCollector\Service;
+
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2019 Dirk Wenzel <wenzel@cps-it.de>
+ *  All rights reserved
+ *
+ * The GNU General Public License can be found at
+ * http://www.gnu.org/copyleft/gpl.html.
+ * A copy is found in the text file GPL.txt and important notices to the license
+ * from the author is found in LICENSE.txt distributed with these scripts.
+ * This script is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+use DWenzel\DataCollector\Entity\Dto\DumpDemand;
+use DWenzel\DataCollector\Service\Dto\ResultInterface;
+
+interface StorageServiceInterface
+{
+    public function dump(DumpDemand $dumpDemand): ResultInterface;
+}
